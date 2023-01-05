ec5b35b049f59811396cf693526da7fee1e72aa7
Roman Aksanov <raksanov.exceedteam@gmail.com>
2023-01-05 18:42:48 +0300

    add class component with input and counter

--- laerning-react/src/App.js
+++ laerning-react/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { ClassCounter } from './components/ClassComponent';
 import Counter from "./components/Counter";
 function App() {
   const [value, setValue] = useState("значение в инпуте");
   return (
     <div className="App">
       <Counter />
+      <ClassCounter/>
     </div>
   );
 }
