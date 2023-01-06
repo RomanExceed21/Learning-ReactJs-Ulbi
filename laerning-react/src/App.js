526e138bda2378f2597819016c16b4294f96097f
Roman Aksanov <raksanov.exceedteam@gmail.com>
2023-01-06 22:47:53 +0300

    add axios, useEffect, loader

--- laerning-react/src/App.js
+++ laerning-react/src/App.js
@@ -1,26 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { usePosts } from './hooks/usePosts';
 import PostForm from './components/PostForm';
 import PostsList from './components/PostsList';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/myModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
+import PostService from './API/PostService';
+import Loader from './components/UI/loader/Loader';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "rer", body: "lkj" },
-    { id: 2, title: "sdsd", body: "iuy" },
-    { id: 3, title: "tutu", body: "zxc" },
-    { id: 4, title: "qwqw", body: "eg" },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({sort: "", query: ""})
   const [visible, setVisible] = useState(false)
-  const sortedAndSerchedPosts = usePosts(posts, filter.sort, filter.query)
+  const sortedAndSerchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostLoading, setIsPostLoading] = useState(false)
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   function createPost(newPost) {
     setPosts([...posts, newPost]);
     setVisible(false)
+  }
+
+  async function fetchPosts () {
+    setIsPostLoading(true)
+    const posts = await PostService.getAll()
+    setPosts(posts);
+    setIsPostLoading(false);
+
   }
 
   function removepost(post) {
@@ -29,24 +39,25 @@ function App() {
 
   return (
     <div className="App">
-      <MyButton 
-        style={{marginTop: "30px"}} 
-        onClick={() => setVisible(true)}
-      >
-        Добавить пользователя
+      <MyButton style={{ marginTop: "30px" }} onClick={() => setVisible(true)}>
+        Добавить пост
       </MyButton>
       <MyModal visible={visible} setVisible={setVisible}>
-        <PostForm
-          create={createPost}
-        />
+        <PostForm create={createPost} />
       </MyModal>
       <hr style={{ margin: "15px" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostsList
-        remove={removepost}
-        posts={sortedAndSerchedPosts}
-        title="Посты про JS"
-      />
+      {isPostLoading ? (
+        <div style={{display: "flex", justifyContent: "center", marginTop: "50px"}}>
+          <Loader />
+        </div>
+      ) : (
+        <PostsList
+          remove={removepost}
+          posts={sortedAndSerchedPosts}
+          title="Посты про JS"
+        />
+      )}
     </div>
   );
 }
